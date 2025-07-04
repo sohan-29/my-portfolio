4c33eea7337ae7e5738ed119fd9cc6bf12b494b7
Sohan <sohan291106@gmail.com>
2025-07-04 19:28:35 +0530

    made header responsive and added burger to toggle navs

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import LandingPage from "./landingPage";
 
 const Header = () => {
+  const [showNavs, setShowNavs] = useState(true);
   return (
-    <div className="flex justify-between px-18 py-3 bg-[#222222] w-full">
-      <span className="mx-9 text-3xl font-bold text-[#cf162e]">@sohan</span>
-      <div className="flex justify-around items-center w-lg">
-        <a href="#" className="text-md text-[#ffffff]">Home</a>
-        <a href="#" className="text-md text-[#ffffff]">About</a>
-        <a href="#" className="text-md text-[#ffffff]">Skills</a>
-        <a href="#" className="text-md text-[#ffffff]">Projects</a>
-        <a href="#" className="text-md text-[#ffffff]">Contact</a>
+    <div className="flex flex-col sm:flex-row justify-between px-5 sm:px-18 py-3 bg-[#222222] w-full">
+      <span className="sm:mr-18 lg:mx-9 sm:text-3xl text-xl font-bold text-[#cf162e] flex justify-between">@sohan
+      <button className="sm:hidden flex flex-col justify-between p-2 h-7 w-8 border-[#dbdbdb] rounded-lg border-1 group" type="button"
+      onClick={() => setShowNavs(!showNavs)}>
+        <span className="block w-full h-[2px] bg-white"></span>
+        <span className="block w-full h-[2px] bg-white"></span>
+        <span className="block w-full h-[2px] bg-white"></span>
+      </button>
+      </span>
+      <div className={"sm:flex flex-col sm:flex-row items-start sm:justify-around sm:items-center w-full sm:w-lg " + (showNavs ? 'flex mt-2 mt-l-1' : 'hidden')}>
+        <a href="#" className="relative group text-sm sm:text-md text-[#ffffff]">Home
+          <span className="sm:block absolute bottom-0 left-0 w-0 h-[1px] bg-white rounded-full transition-all duration-300 group-hover:w-full"></span></a>
+        <a href="#" className="relative group text-sm sm:text-md text-[#ffffff]">About
+          <span className="sm:block absolute bottom-0 left-0 w-0 h-[1px] bg-white rounded-full transition-all duration-300 group-hover:w-full"></span></a>
+        <a href="#" className="relative group text-sm sm:text-md text-[#ffffff]">Skills
+          <span className="sm:block absolute bottom-0 left-0 w-0 h-[1px] bg-white rounded-full transition-all duration-300 group-hover:w-full"></span></a>
+        <a href="#" className="relative group text-sm sm:text-md text-[#ffffff]">Projects
+          <span className="sm:block absolute bottom-0 left-0 w-0 h-[1px] bg-white rounded-full transition-all duration-300 group-hover:w-full"></span></a>
+        <a href="#" className="relative group text-sm sm:text-md text-[#ffffff]">Contact
+          <span className="sm:block absolute bottom-0 left-0 w-0 h-[1px] bg-white rounded-full transition-all duration-300 group-hover:w-full"></span></a>
       </div>
     </div>
   )
