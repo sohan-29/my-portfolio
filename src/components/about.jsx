be7d1b3bff4e6c7263b2473d755d32ad8a921e5f
Sohan <sohan291106@gmail.com>
2025-07-31 17:13:52 +0530

    added profile to about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,9 @@
 const AboutMe = () => {
     return (
         <section className="flex flex-col sm:flex-row items-center justify-center gap-11 bg-[#333333ff] text-white p-5 px-[7vw] min-h-169">
-            <div className="w-3/9 text-center sm:text-left">
-                <img src="https://sohan-29.github.io/my-portfolio/aboutProfile.png" alt="About Me" className="w-md rounded-lg shadow-lg" />
+            <div className="relative w-3/9 aspect-square text-center sm:text-left">
+                {/* <div className="absolute top-10 left-6 w-96 h-96 border-2 text-[#ca1931ea] rounded-full"></div> */}
+                <img src="https://sohan-29.github.io/my-portfolio/aboutProfile.png" alt="About Me" className="w-sm mt-6 rounded-full border-4 text-[#ca1931ea] shadow-[0_0_11px_#ca1931ea,0_0_29px_#ca1931ef]" />
             </div>
             <div className="w-6/9 text-center sm:text-left text-[#9f9f9f]">
                 <h2 className="text-4xl text-[#efefef] font-bold mb-3">About Me</h2>
