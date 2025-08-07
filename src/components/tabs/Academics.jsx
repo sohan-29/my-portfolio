dd86ece8053d2f0ab855fea4b60d04db0565feee
Sohan <sohan291106@gmail.com>
2025-08-07 18:14:57 +0530

    modified responsiveness

--- src/components/tabs/Academics.jsx
+++ src/components/tabs/Academics.jsx
@@ -25,11 +25,11 @@ const Academics = () => {
           {console.log(item)}
           <h2 className="text-base md:text-lg lg:text-xl text-[#efefefbf] font-bold">{item.InstituteName}</h2>
           <p className="text-xs md:text-sm lg:text-base text-[#999999] italic">{item.course}</p>
-          <p className="text-sm text-[#ca1931] font-semibold">
+          <p className="text-xs sm:text-sm md:text-base lg:text-lg text-[#ca1931] font-semibold">
             {item.startYear} - {item.endYear <= new Date().getFullYear() ? item.endYear : "present"}
           </p>
-          <p className="mt-2 text-sm md:text-base">{item.discription1}</p>
-          <p className="mt-1 text-sm md:text-base">{item.discription2 }</p>
+          <p className="mt-2 text-xs sm:text-sm lg:text-base xl:text-lg">{item.discription1}</p>
+          <p className="mt-1 text-xs sm:text-sm lg:text-base xl:text-lg">{item.discription2 }</p>
         </div>
       ))}
     </div>
