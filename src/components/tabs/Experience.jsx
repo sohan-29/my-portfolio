7c52dca02e49b3d81ab40a271613a4d59130179b
Sohan <sohan291106@gmail.com>
2025-08-09 18:53:51 +0530

    integrated api

--- src/components/tabs/Experience.jsx
+++ src/components/tabs/Experience.jsx
@@ -1,9 +1,25 @@
-const Experience = ({ aboutData }) => {
-  return (
-    <div className="tab-content mt-6">
-      <h3 className="text-2xl font-bold mb-4 text-white">Experience</h3>
-    </div>
-  );
-};
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default Experience;
+const Experience = () => {
+  const [expData, setExpData] = useState([]);
+  useEffect(() => {
+    const fetchExpData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/api/experience");
+        setExpData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching experience data:", error);
+      }
+    }
+    fetchExpData();
+  }, []);
+  return (
+        <div className="tab-content mt-6">
+          <h3 className="text-2xl font-bold mb-4 text-white">Experience</h3>
+        </div>
+      );
+    };
+
+    export default Experience;
