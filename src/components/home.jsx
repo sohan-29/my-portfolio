1517e879d4e8405c74a58da647e41349ed7cce29
Sohan <sohan291106@gmail.com>
2025-08-13 19:03:41 +0530

    redirects to home page while clicking logo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,14 +7,15 @@ export const Header = () => {
 
   return (
     <div className="flex flex-col sm:flex-row justify-between px-5 sm:px-18 py-3 bg-[#222222] w-full z-50 top-0 sticky">
-      <span className="sm:mr-18 lg:mx-9 sm:text-3xl text-xl font-bold text-[#cf162e] flex justify-between cursor-pointer">@sohan
+      <div className="sm:mr-18 lg:mx-9 sm:text-3xl text-xl font-bold text-[#cf162e] flex justify-between cursor-pointer">
+        <a href="/">@Sohan</a>
       <button className="sm:hidden flex flex-col justify-between p-2 h-7 w-8 border-[#dbdbdb] rounded-lg border-1 group" type="button"
       onClick={() => setShowNavs(!showNavs)}>
         <span className="block w-full h-[2px] bg-white"></span>
         <span className="block w-full h-[2px] bg-white"></span>
         <span className="block w-full h-[2px] bg-white"></span>
       </button>
-      </span>
+      </div>
       <div className={"sm:flex flex-col sm:flex-row items-start sm:justify-around sm:items-center w-full sm:w-lg " + (showNavs ? 'flex mt-2 mt-l-1' : 'hidden')}>
         <a href="/" className="relative group text-sm sm:text-md text-[#ffffff]">Home
           <span className="sm:block absolute bottom-0 left-0 w-0 h-[1px] bg-white rounded-full transition-all duration-300 group-hover:w-full"></span></a>
