8fef14942005fd12d766d891b7c785ee49864f19
Sohan <sohan291106@gmail.com>
2025-08-13 18:09:03 +0530

    modified styles

--- src/components/tabs/Experience.jsx
+++ src/components/tabs/Experience.jsx
@@ -19,9 +19,9 @@ const Experience = () => {
         <div className="tab-content mt-6">
           {expData.map((item, index) => (
             <div key={index} className="mb-8">
-              <div className="flex flex-col w-fit item-center justify-between sm:gap-3 w-full">
+              <div className="flex flex-col sm:flex-row w-full sm:w-fit item-center justify-between sm:gap-3">
               <h2 className="text-lg md:text-xl lg:text-2xl text-[#efefefbf] font-bold">{item.company}</h2>
-              <p className="text-xs sm:text-sm text-[#999999] md:mt-1.5 xl:mt-2.5">{item.location}</p>
+              <p className="text-xs sm:text-sm text-[#999999] mt-0 md:mt-1.5 lg:mt-2 xl:mt-2.5">{item.location}</p>
               </div>
               <p className="text-sm md:text-sm lg:text-base text-[#999999] italic">{item.title}</p>
               <p className="text-xs sm:text-sm lg:text-base text-[#ca1931] font-semibold">
