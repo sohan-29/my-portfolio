91fe2aad12d7020b6879d300ac15ebc849ed08cc
Sohan <sohan291106@gmail.com>
2025-08-20 18:33:57 +0530

    installed daisyui plugin

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -6,6 +6,7 @@ const Skills = () => {
       {location.pathname.includes("skills") && <Header />}
       <div className="flex flex-col p-4 shadow-md bg-[#3b3b3bfe] text-white px-[7vw] h-full min-h-screen">
         <h2 className="text-[#eeeeeeee] text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-2 mx-[1vw] mt-18 border-b-2">Skills</h2>
+        <div className="radial-progress" style={{ "--value": 90 }} aria-valuenow={90} role="progressbar">html</div>
       </div>
     </>
   );
