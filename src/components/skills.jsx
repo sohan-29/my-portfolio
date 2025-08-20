7d7a4de73c994450008936c3365d20cfc912e2ae
Sohan <sohan291106@gmail.com>
2025-08-20 18:52:39 +0530

    created custom radial progress

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,8 +5,12 @@ const Skills = () => {
     <>
       {location.pathname.includes("skills") && <Header />}
       <div className="flex flex-col p-4 shadow-md bg-[#3b3b3bfe] text-white px-[7vw] h-full min-h-screen">
-        <h2 className="text-[#eeeeeeee] text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-2 mx-[1vw] mt-18 border-b-2">Skills</h2>
-        <div className="radial-progress" style={{ "--value": 90 }} aria-valuenow={90} role="progressbar">html</div>
+        <h2 className="text-[#eeeeeebe] text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-2 mx-[1vw] mt-18 border-b-2">Skills</h2>
+        <div className="relative">
+          <div className="radial-progress text-[#da1931fe] rotate-90" style={{ "--value": 90, "--size": "5rem" }} aria-valuenow={90} role="progressbar">
+          </div>
+          <span className="absolute left-5 top-6.5 text-[#eeeeeedb] mb-1">HTML</span>
+        </div>
       </div>
     </>
   );
