16773d681ba694b7b18939489c5f2d80e6a7ac7e
Sohan <sohan291106@gmail.com>
2025-08-21 18:53:48 +0530

    modified responsiveness for skills page

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -6,7 +6,7 @@ const RadialProgress = ({ key, skill, icon }) => {
   return (
     <div key={key} className="relative">
       <div className="radial-progress text-[#4e4e4e] rotate-90" style={{ "--value": 100, "--size": "5rem" }} aria-valuenow={90} role="progressbar"></div>
-      <img className="absolute left-10 top-9.5 -translate-x-1/2 -translate-y-1/2 w-8 h-8" src={icon} alt={`${skill} icon`} />
+      <img className="absolute left-10 top-10 -translate-x-1/2 -translate-y-1/2 w-8 h-8" src={icon} alt={`${skill} icon`} />
       {/* <span className="absolute left-6 top-6.5 -translate-x-1/2 -translate-y-1/2 text-[#eeeeeedb] font-bold text-sm">
         {value}%
       </span> */}
@@ -35,7 +35,7 @@ const Skills = () => {
       {location.pathname.includes("skills") && <Header />}
       <div className="flex flex-col p-4 shadow-md bg-[#3b3b3bfe] text-white px-[7vw] h-full min-h-screen">
         <h2 className="text-[#eeeeeebe] text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-2 mx-[1vw] mt-18 border-b-2">Skills</h2>
-        <div className="flex flex-wrap justify-center items-center gap-11 mt-11 mb-10">
+        <div className="flex flex-wrap justify-center items-center gap-3 md:gap-6 lg:gap-9 xl:gap-11 mt-11 mb-10 w-[88%] mx-auto">
           {skills.map((item, index) => (
             <RadialProgress key={index} icon={item.icon} skill={item.skill} />
           ))}
